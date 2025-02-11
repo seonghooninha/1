57bdd737815cdd046a545882940052c18590498b
김성훈 <soenghoon123@naver.com>
2025-02-11 12:50:04 +0900

    v0.4

--- day02.py
+++ day02.py
@@ -2,6 +2,25 @@
 #    for i in range(1, 10, 1):
 #        print(f"{dan} * {i} = {dan*i}")
 
-dan = input("Input dan : ")
-for i in range(1, 10, 10):
-    print(f"{dan}")
+#dan = input("Input dan : ")
+#for i in range(1, 10, 10):
+#    print(f"{dan}")
+
+n = int(input("Input number :"))
+
+is_prime = True
+
+if n>=2:
+    for i in range(2, int(n**0.5) +1):
+        if n %i == 0:
+            # count = count +1
+        is_prime = False
+        break
+        print(i, end=' ')
+else:
+    is_prime = False
+
+if is_prime == True:
+    print(f"{n} is prime number")
+else:
+    print(f"{n} is NOt git prime number")
