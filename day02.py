c166f433ac740d39430eb4608ebd9901d037dff4
김성훈 <soenghoon123@naver.com>
2025-02-11 10:48:27 +0900

    v0.4

---
+++ day02.py
@@ -0,0 +1,7 @@
+#for dan in range (2, 10, 1):
+#    for i in range(1, 10, 1):
+#        print(f"{dan} * {i} = {dan*i}")
+
+dan = input("Input dan : ")
+for i in range(1, 10, 10):
+    print(f"{dan}")
